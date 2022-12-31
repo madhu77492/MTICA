9e5fee07a94e8a0f81f5708431d0003cd5d65d8e
madhu77492 <121157349+madhu77492@users.noreply.github.com>
2022-12-31 17:08:00 +0530

    Add files via upload

--- program_10.py
+++ program_10.py
@@ -1,8 +1,17 @@
-num1=int(input("Enter a no:"))
-if num1%2==0:
-    print(num1, 'is even')
-
-if num1%2==1:
-    print(num1, 'is odd')
-
-print("We  learnt if keyword")
+import sqlite3
+carData=[
+    (1, 'Audi', 52642),
+    (2, 'Mercedes', 52643),
+    (3, 'Volvo', 52644),
+    (4, 'Citroen', 52646),
+    (5, 'Hummer', 52645),
+    (6, 'Volkswagen', 52649)
+    ]
+con=sqlite3.connect('mtica.db')
+cur=con.cursor()
+cur.execute("DROP TABLE IF EXISTS Cars")
+cur.execute("CREATE TABLE Cars(Id INT,Name TEXT,Price INT)")
+cur.executemany("INSERT INTO Cars VALUES(?,?,?)",carData)
+con.commit()
+con.close()
+print("Values inserted.")
